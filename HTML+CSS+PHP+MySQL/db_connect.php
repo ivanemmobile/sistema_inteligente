abd7228a521f3b56f1055ab870af0be9f5314e3f
_ivanpaulino <147009428+ivanemmobile@users.noreply.github.com>
2023-10-24 00:03:32 -0300

    Update db_connect.php

--- HTML+CSS+PHP+MySQL/db_connect.php
+++ HTML+CSS+PHP+MySQL/db_connect.php
@@ -1,6 +1,7 @@
 <?php 
 /*
-Configuração da conexao com o banco de dados onde sera definido o nome do servidor, banco de dados e tabela com os itens por colunas.
+Configuração da conexao com o banco de dados onde sera definido o nome do servidor, banco de 
+dados e tabela com os itens por colunas.
 */
 $username = "root"; //definição do usuario.
 $servername = "localhost"; //local de acesso.
